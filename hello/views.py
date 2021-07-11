aded01bb2e1dffca1732f1c87220b48fb202a876
Akash0811 <aakashbiswal@gmail.com>
2021-07-12 02:47:58 +0530

    Float Error in Price

--- hello/views.py
+++ hello/views.py
@@ -37,9 +37,9 @@ def db(request):
     num = NumRegex.findall(string1)
     
     # Add the entry to the table
-    alert = Alert(d1 = date_string,
+    alert = Alert.objects.create(d1 = date_string,
                   t1 = time_string,
-                  price = num[0])
+                  price = float(num[0]))
     alert.save()
 
     alerts = Alert.objects.all()
